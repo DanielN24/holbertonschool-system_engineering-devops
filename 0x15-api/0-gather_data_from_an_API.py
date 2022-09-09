1410eb2da165e90728a01b140cd00314a1b750c2
DanielN24 <capdaniel_98@outlook.com>
2022-09-09 12:04:49 -0500

    answer number 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,8 @@
 using this REST API, for a given employee ID
 returns information about his/her TODO list progress
 """
-import requests
 from sys import argv
+import requests
 
 
 if __name__ == "__main__":
